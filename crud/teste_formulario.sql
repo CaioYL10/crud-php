bfa3c162d874f2898d5d1a5e527fb06aaff34c73
Caio Yuri Lima <caioyl15.ask@gmail.com>
2025-11-07 16:23:35 -0300

    Add files via upload

---
+++ crud/teste_formulario.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 07/11/2025 às 19:04
+-- Versão do servidor: 8.0.40
+-- Versão do PHP: 8.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `teste_formulario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `carros`
+--
+
+CREATE TABLE `carros` (
+  `id` int NOT NULL,
+  `modelo` varchar(255) DEFAULT NULL,
+  `ano` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `carros`
+--
+
+INSERT INTO `carros` (`id`, `modelo`, `ano`) VALUES
+(14, 'Onix', 2012);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `carros`
+--
+ALTER TABLE `carros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `carros`
+--
+ALTER TABLE `carros`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
