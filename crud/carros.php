02132a59729e65fb8c8614dff3baac53e98e845d
Caio Yuri Lima <caioyl15.ask@gmail.com>
2025-11-07 16:22:48 -0300

    Delete crud directory

--- crud/carros.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $sql = "CREATE TABLE IF NOT EXISTS Carros (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        modelo VARCHAR(255),
-        ano INT
-    )";
-    $conn->query($sql);
-
-?>
